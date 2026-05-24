6b445348a1632643927cecbb2f259177f52090d8
Narisnan <tanawayhome@gmail.com>
2026-05-25 03:04:26 +0700

    ScriptMovement

--- Assets/Script/PlayeMovement.cs
+++ Assets/Script/PlayeMovement.cs
@@ -1,16 +1,98 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayeMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] InputActionAsset inputActions;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float rotationSpeed = 12f;
+    [SerializeField] bool cameraRelative = true;
+
+    Rigidbody rb;
+    InputAction moveAction;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.constraints |= RigidbodyConstraints.FreezeRotation;
+
+        if (inputActions != null)
+            moveAction = inputActions.FindActionMap("Player").FindAction("Move");
+        else
+            moveAction = CreateFallbackMoveAction();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        moveAction?.Enable();
+    }
+
+    void OnDisable()
+    {
+        moveAction?.Disable();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        if (input.sqrMagnitude > 1f)
+            input.Normalize();
+
+        Vector3 velocity = rb.linearVelocity;
+
+        if (input.sqrMagnitude < 0.01f)
+        {
+            velocity.x = 0f;
+            velocity.z = 0f;
+            rb.linearVelocity = velocity;
+            return;
+        }
+
+        Vector3 direction = GetMoveDirection(input);
+        velocity.x = direction.x * moveSpeed;
+        velocity.z = direction.z * moveSpeed;
+        rb.linearVelocity = velocity;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime));
+    }
+
+    Vector3 GetMoveDirection(Vector2 input)
+    {
+        Vector3 forward = Vector3.forward;
+        Vector3 right = Vector3.right;
+
+        if (cameraRelative && Camera.main != null)
+        {
+            forward = Camera.main.transform.forward;
+            right = Camera.main.transform.right;
+            forward.y = 0f;
+            right.y = 0f;
+            forward.Normalize();
+            right.Normalize();
+        }
+
+        return (forward * input.y + right * input.x).normalized;
+    }
+
+    static InputAction CreateFallbackMoveAction()
+    {
+        var move = new InputAction("Move", InputActionType.Value);
+
+        move.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard>/s")
+            .With("Left", "<Keyboard>/a")
+            .With("Right", "<Keyboard>/d");
+
+        move.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/upArrow")
+            .With("Down", "<Keyboard>/downArrow")
+            .With("Left", "<Keyboard>/leftArrow")
+            .With("Right", "<Keyboard>/rightArrow");
+
+        move.AddBinding("<Gamepad>/leftStick");
+        return move;
     }
 }
